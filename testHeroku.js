5cbcb4be8ea48eca0edff2c42cc2ef9ab6ef67f7
shagufta2107 <shagufta.zhcet09@gmail.com>
2017-11-01 09:20:25 +0530

    Adding test heroku file

---
+++ testHeroku.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function (req,res){
+	fs.readFile('form.html', function (err, data) {
+			res.writeHead(200, {
+				'Content-Type': 'text/html',
+					'Content-Length': data.length
+			});
+			res.write("Welcome to Shagufta's page");
+			res.end();
+		});
+});
+
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
