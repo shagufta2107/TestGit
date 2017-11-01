c57760d16faf60884b9422f8e007edba699bb492
shagufta2107 <shagufta.zhcet09@gmail.com>
2017-11-01 08:36:33 +0530

    first commit

---
+++ readData.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+
+app.get('/home', function (req,res){
+	fs.readFile('form.html', function (err, data) {
+			res.writeHead(200, {
+				'Content-Type': 'text/html',
+					'Content-Length': data.length
+			});
+			res.write(data);
+			res.end();
+		});
+});
+
+app.post('/home', function (req,res){
+	MongoClient.connect(url, function(err, db) {
+	  if (err) throw err;
+		
+	  db.collection("customers").find({}).toArray(function(err, result) {
+		if (err) throw err;
+		console.log(result);
+		res.send(result)
+		db.close();
+	  });	
+	});
+});
+
+
+
+var server = app.listen(8085, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+});
