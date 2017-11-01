3e0ee781a55a72874a1bbbd546ba2089fca66fe9
RH <raihaanhameedi@gmail.com>
2017-11-01 21:41:50 +0530

    make it better

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ var fs = require("fs");
 
 
 app.set('port', (process.env.PORT || 5000));
-
-app.get('', function (req,res){
-			res.end("fjgdfhd");
-			//return res.send("Welcome to Shagufta's page");
-		});
+app.get('/', function(request, response) {
+response.writeHead(200, {
+				'Content-Type': 'text/html'
+			});
+response.end("al");
 });
-
-
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
