90b3f6d6c9babe60ae72bf1264aa03aeb95dd3ae
shagufta2107 <shagufta.zhcet09@gmail.com>
2017-11-04 10:21:43 +0530

    adding create

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
 var fs = require("fs");
+
+
 MongoClient = require('mongodb').MongoClient;
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
 
 MONGO_URL = 'mongodb://testUser1:tester123@ds245615.mlab.com:45615/shagufta_database';
 /*
@@ -32,18 +41,47 @@ MongoClient.connect(MONGO_URL, (err, db) => {
 }); */
 
 app.set('port', (process.env.PORT || 5000));
-app.get('/sa', function(request, response) {
+app.get('/create', function(request, response) {
 	fs.readFile('form.html', function (err, data) {
 				response.writeHead(200, {
-					'Content-Type': 'text/html'
-						
+					'Content-Type': 'text/html',
+					'Content-Length': data.length	
 				});
 				response.write(data);
 				response.end();
 			});
 	});
 
-
+app.post('/create',function(req,res)
+{
+	console.log(req.body);
+	//console.log(req.body.category);
+	//console.log(req.body.mainCategory);
+	
+	MongoClient.connect(MONGO_URL, function(err, db)
+	{
+		if (err) throw err;
+			
+		 db.collection('ProductCollection').insertOne(
+		{
+		    ProductId: (req.body.productId),
+			Category: (req.body.category),
+			MainCategory:(req.body.mainCategory)
+		},
+		function (err, res)
+		{
+			if (err)
+			{
+				db.close();
+				return console.log(err);
+			}
+		});
+	  
+	});
+	res.send(req.body);
+	
+});
+	
 app.get('/products', function (req,res){
 	
 	
